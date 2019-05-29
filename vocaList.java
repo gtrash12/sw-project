4104e832cac9cb04c4f43fd85d179c356cd72b0c
gtrash12 <gtrash12@naver.com>
2019-05-29 14:21:02 +0900

    Update vocaList.java
    Add 2 func

--- vocaList.java
+++ vocaList.java
@@ -2,6 +2,13 @@ package com.example.basic.voca;
 
 import java.util.Vector;
 
-public class vocaList extends  Voca{
+public class vocaList {
     Vector<Voca> list = new Vector<Voca>();
+    
+    void addVoca(Voca ele){
+        list.addElement(ele);
+    }
+    void removeVoca(Voca ele){
+        list.remove(ele);
+    }
 }
