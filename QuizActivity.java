9e169ff40e929192df48a09294f59cd7bf1caae5
kimtjddnr <38129927+kimtjddnr@users.noreply.github.com>
2019-06-18 14:45:59 +0900

    퀴즈 구현

---
+++ QuizActivity.java
@@ -0,0 +1,57 @@
+package com.example.vocaquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    TextView vocareText;
+    EditText vocainEdit;
+    Button submitButton;
+
+    Voca voca_receiver;
+
+    static int numoftry = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        vocareText = (TextView) findViewById(R.id.voca_receiver);
+        vocainEdit = (EditText) findViewById(R.id.voca_input);
+        submitButton = (Button) findViewById(R.id.voca_submit);
+
+        voca_receiver = Vocalist.getInstance().getRandom();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                while (numoftry < 10) {
+                    if (compareVoca() == 1) {
+                        Toast.makeText(getApplicationContext(), "정답!!", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    else
+                        Toast.makeText(getApplicationContext(), "오답!!", Toast.LENGTH_LONG).show();
+                }
+                if(numoftry==10)
+                    Toast.makeText(getApplicationContext(), "실패!! 잠다깼쥬~?", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public int compareVoca() {
+        numoftry++;
+        if (voca_receiver.getWord() == vocainEdit.getText().toString())
+            return 1;
+        else
+            return 0;
+    }
+}
+
+
